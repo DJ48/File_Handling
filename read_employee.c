5958e1e7bd3c7000331e9a4c4cde10e8ddd88fbf
Deepak Jaiswal <35431078+DJ48@users.noreply.github.com>
2021-02-25 13:05:07 +0530

    Create read_employee.c

---
+++ read_employee.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Employee{
+    char empid[5];
+    char empname[20];
+    char company[20];
+    struct Employee *next;
+};
+
+typedef struct Employee emp;
+emp *head=NULL;
+
+int main()
+{
+    FILE *fp;
+    
+    create();
+    
+    fp=fopen("EmployeeDB","rb");
+    fwrite(&head,sizeof(head),1,fp);
+    printf("\n--------------Printing Details--------------------\n");
+    while(head != NULL){
+        printf("\nEmployee ID:- %s",head->empid);
+        printf("\tEmployee Name:- %s",head->empname);
+        printf("\tEmployee company:- %s",head->company);
+        head=head->next;
+    }
+    fclose(fp);
+    return 0;
+}
