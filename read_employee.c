4deac56b1d17a1031105ff65716bc3d368f2208e
Deepak Jaiswal <35431078+DJ48@users.noreply.github.com>
2021-02-25 15:18:11 +0530

    Update read_employee.c

--- read_employee.c
+++ read_employee.c
@@ -15,15 +15,12 @@ int main()
     FILE *fp;
     emp *head=(emp *)malloc(sizeof(emp));
     fp=fopen("EmployeeDB","rb");
-    fread(head,sizeof(*head),1,fp);
     printf("\n--------------Printing Details--------------------\n");
-    for(int i=0;i<3;i++{
-        
-    while(head != NULL){
-        printf("\nEmployee ID:- %s",head->empid);
-        printf("\tEmployee Name:- %s",head->empname);
-        printf("\tEmployee company:- %s",head->company);
-        head=head->next;
+    while((fread(head,sizeof(*head),1,fp)==1){
+            printf("\nEmployee ID:- %s",head->empid);
+            printf("\tEmployee Name:- %s",head->empname);
+            printf("\tEmployee company:- %s",head->company);
+            head=head->next;
     }
     fclose(fp);
     return 0;
