909255b9e51366ff2299e67729aca3c5605bc9be
Deepak Jaiswal <35431078+DJ48@users.noreply.github.com>
2021-02-26 11:50:26 +0530

    Update employee_writing.c

--- employee_writing.c
+++ employee_writing.c
@@ -1,59 +1,26 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 struct Employee{
     char empid[5];
     char empname[20];
-    char company[20];
-    struct Employee *next;
+    char company;
 };
 
-typedef struct Employee emp;
-emp *head=NULL;
-
-emp *createnode(){
-    emp *newnode = (emp *)malloc(sizeof(emp));
-    printf("Enter the Employee Id:- ");
-    scanf("%s",newnode->empid);
-    printf("Enter the Name:- ");
-    scanf("%s",newnode->empname);
-    printf("Enter the company:- ");
-    scanf("%s",newnode->company);
-    newnode->next=NULL;
-    return newnode;
-}
-
-void create(){
+int main(){
+    struct Employee emp[3];
     int i;
-    for(i=0;i<3;i++){
-        if(i==0){
-            head=createnode();
-        }
-        else{
-            emp *temp=head;
-            while(temp->next != NULL){
-                temp = temp->next;
-            }
-            temp->next=createnode();
-        }
-    }
-}
-
-int main()
-{
     FILE *fp;
-    printf("Start writing into file\n");
-    create();
-    
     fp=fopen("EmployeeDB","wb");
-    
-    emp *temp=head;
-    while(temp != NULL){
-        fwrite(temp,sizeof(emp),1,fp);
-        temp=temp->next;
+    for(i=0;i<3;i++){
+        printf("\nEnter Id:- ");
+        scanf("%s",emp[i].empid);
+        printf("\nEnter Name:- ");
+        scanf("%s",emp[i].empname);
+        printf("\nEnter Company Name:- ");
+        scanf("%s",emp[i].company);
+        fwrite(&emp[i],sizeof(struct Employee),1,fp);
     }
-    fputs("Hello DJ",fp);
-    printf("writing Done into EmployeeDB\n");
-    fclose(fp);
+    printf("Writing into EmployeeDB Done....\n");
+    
     return 0;
 }
