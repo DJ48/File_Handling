b8401a67e8324e50a467f59fe02692fd7c9d6538
Deepak Jaiswal <35431078+DJ48@users.noreply.github.com>
2021-02-26 12:19:35 +0530

    Update f1.c

--- f1.c
+++ f1.c
@@ -15,7 +15,7 @@ struct person
 int main () 
 { 
 	FILE *outfile; 
-	
+	struct person input[2];
 	// open file for writing 
 	outfile = fopen ("person.dat", "w"); 
 	if (outfile == NULL) 
@@ -23,13 +23,13 @@ int main ()
 		fprintf(stderr, "\nError opend file\n"); 
 		exit (1); 
 	} 
-
-	struct person input1 = {1, "rohan", "sharma"}; 
-	struct person input2 = {2, "mahendra", "dhoni"}; 
+		
+	struct person input[0] = {1, "rohan", "sharma"}; 
+	struct person input[1] = {2, "mahendra", "dhoni"}; 
 	
 	// write struct to file 
-	fwrite (&input1, sizeof(struct person), 1, outfile); 
-	fwrite (&input2, sizeof(struct person), 1, outfile); 
+	fwrite (&input[0], sizeof(struct person), 1, outfile); 
+	fwrite (&input[1], sizeof(struct person), 1, outfile); 
 	
 	if(fwrite != 0) 
 		printf("contents to file written successfully !\n"); 
